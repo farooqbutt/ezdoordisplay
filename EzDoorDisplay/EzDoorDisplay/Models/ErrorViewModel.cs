70c93954c4ff3a790e8727086c730cd021be69ab
Farooq Ahmad <farooq.ahmad35202@gmail.com>
2023-12-03 17:24:30 +0500

    Latest Code Changes first commit.

---
+++ EzDoorDisplay/EzDoorDisplay/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EzDoorDisplay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
