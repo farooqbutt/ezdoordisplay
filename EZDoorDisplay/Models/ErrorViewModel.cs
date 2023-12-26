550b1bced04a148e32c9a41ccebdd2bf3753fea0
Farooq Ahmad <farooq.ahmad35202@gmail.com>
2023-12-26 22:10:08 +0500

    upgraded webapp framework from .net core 6.0 to .net core 8.0

---
+++ EZDoorDisplay/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EZDoorDisplay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
